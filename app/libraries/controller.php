b22e5099fe6e6563616c180c8d2d84a3f7ac1db2
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-16 16:25:05 +0530

    added base controller

---
+++ app/libraries/controller.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+ * Base Controller
+ * Loads the models and views
+ */
+ class Controller
+ {
+    // Load model
+    public function model($model)
+    {
+        // Require model file
+        require_once '../app/models/'.$model.'.php';
+
+        // instantiate model
+        require new $model();
+    }
+
+    // Load view
+    public function view($view, $data = [])
+    {
+        // Check if view file exits
+        if(file_exists('../app/views/'.$view.'.php')){
+            require_once '../app/views/'.$view.'.php';
+        }else{
+            // View doesnot exists
+            die('View does not exists');
+        }
+    }
+ }
